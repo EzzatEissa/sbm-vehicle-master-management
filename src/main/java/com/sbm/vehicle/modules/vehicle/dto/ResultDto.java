a180c7f8c19099b600c557a5e573698de36a7fd6
EzzatAEissa <ezzat.a.eissa@gmail.com>
2020-12-03 07:01:10 +0300

    vehicle

---
+++ src/main/java/com/sbm/vehicle/modules/vehicle/dto/ResultDto.java
@@ -0,0 +1,32 @@
+package com.sbm.vehicle.modules.vehicle.dto;
+
+public class ResultDto {
+
+    private String type;
+    private String code;
+    private String msg;
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+}
